ba2f741e75715db14bddea39797c14700648c4b0
Nick <nicholasjohnrichardson@gmail.com>
2018-05-15 21:19:40 +0100

    Adds working? method to Bike class

--- spec/Bike_spec.rb
+++ spec/Bike_spec.rb
@@ -1,6 +1,7 @@
-require 'Bike'
+require 'Bike.rb'
 
 describe Bike do
 
+  it { is_expected.to respond_to(:working?) }
 
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -2,7 +2,7 @@ require 'DockingStation.rb'
 
 describe DockingStation do
 
-  it { is_expected.to respond_to(release_bike) }
+  it { is_expected.to respond_to(:release_bike) }
 
   # it "should respond to release_bike" do
   #   expect(DockingStation).to respond_to(release_bike)
