5da405be24cf0792f90733ab7aebdbf4d2446b3d
Nick <nicholasjohnrichardson@gmail.com>
2018-05-16 17:06:19 +0100

    raises error when docking station is full

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -4,15 +4,13 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
-    Bike.new
+    fail 'No bikes available' unless @bike
+    @bike
   end
 
   def dock(bike)
+    fail 'Docking station full' if @bike
     @bike = bike
-  end
-
-  def bike
-    @bike
   end
 
 end
