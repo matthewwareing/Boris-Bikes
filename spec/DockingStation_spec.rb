6204ebdd769e0d047b042ba1320a669fd1370800
Nick <nicholasjohnrichardson@gmail.com>
2018-05-16 13:23:51 +0100

    Bike working? method returns true, require_relative links Bike and DockingStation, DockingStation release_bike method returns new bike

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -4,6 +4,10 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:release_bike) }
 
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end 
   # it "should respond to release_bike" do
   #   expect(DockingStation).to respond_to(release_bike)
   # end
